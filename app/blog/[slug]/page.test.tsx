a0203907ca0a14a8da34974e78fab699f50c6837
Nguyen Hoang Long <longnh.uit@gmail.com>
2025-07-12 14:18:18 +0700

    fix cicd

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -5,7 +5,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Page from "./page";
 
-it("App Router: Works with dynamic route segments", () => {
-  render(<Page params={{ slug: "Test" }} />);
+it("App Router: Works with dynamic route segments", async () => {
+  render(await Page({ params: Promise.resolve({ slug: "Test" }) }));
   expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
 });
