60e5574f8b25d785b086f5234df58ef94c3d7554
Nguyen Hoang Long <longnh.uit@gmail.com>
2025-07-12 14:37:23 +0700

    fi deploy github page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,6 +9,14 @@ export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${slug}` };
 }
 
+export async function generateStaticParams() {
+  return [
+    { slug: 'first-post' },
+    { slug: 'second-post' },
+    { slug: 'third-post' },
+  ];
+}
+
 export default async function Page({ params }: Params) {
   const { slug } = await params;
   return <h1>Slug: {slug}</h1>;
